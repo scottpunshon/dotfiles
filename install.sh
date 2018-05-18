576833d745fa2d32c1a20b13a138b4821b2b18a2
Scott K Punshon <scott.punshon@gmail.com>
2018-05-18 12:33:44 +1000

    Fix install script to not add accidental recursive folder symlinks

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ DOTFILES_HOME_RELPATH="${DOTFILES_SYMLINK/$HOME\//}"
 dryrun=0
 while [ $# -gt 0 ]; do
 	case "$1" in
-		--dryrun)
+		--dryrun|--dry-run)
 			echo "Dry-run: not performing any changes"
 			dryrun=1
 			;;
@@ -29,12 +29,16 @@ symlink() {
 
 	if [ "$dryrun" -ne 0 ]; then
 		return
-	elif [ "$link" -ef "$target" ]; then 
+	elif [ "$link" -ef "$target" ]; then
 		# link/file already refers to target
+		# NOTE: this doesn't come out as true when passing target is relative to $HOME
 		return
 	elif [ -e "$link" -a ! -L "$link" ]; then
 		# exists and isn't a symbolic link, so backup!
 		mv "$link" "${link}.orig"
+	elif [ -e "$link" -a -L "$link" ]; then
+		# exists and is a symbolic link, so remove to replace with ours
+		rm -f "$link"
 	fi
 
 	#ln -si "$target" "$link"  # prompt for overwrite (of existing symbolic links)
@@ -50,6 +54,16 @@ link_dotfiles() {
 	done
 }
 
+link_dirs() {
+	symlink "${_DIRPATH}" "${DOTFILES_SYMLINK}"
+
+	cd "${_DIRPATH}" || exit 1
+	for f in bin ; do
+		symlink "${DOTFILES_HOME_RELPATH}/${f}" "${HOME}/$(basename "$f")"
+	done
+}
+
 
 link_dotfiles
+link_dirs
 
